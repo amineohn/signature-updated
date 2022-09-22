10534ce808c973b3082a71ab02a8ed4a63a091b1
amineohn <amineprojet7@gmail.com>
2022-09-22 15:57:15 +0200

    fix: called or null

--- pages/api/preview.ts
+++ pages/api/preview.ts
@@ -4,7 +4,9 @@ import { NextApiRequest, NextApiResponse } from "next";
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   const index = fs.readFileSync(`./public/static/default/index.html`, `utf8`);
-  const replacedIndex = index
+
+  const fixedIndex = index.replace(/\.link\(\)/g, ``);
+  const replacedIndex = fixedIndex
     .replace(`{{firstName}}`, req.body.firstName)
     .replace(`{{lastName}}`, req.body.lastName)
     .replace(`{{function}}`, req.body.function)
@@ -13,23 +15,12 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
     .replace(`{{number}}`, req.body.number)
     .replace(`{{link}}`, req.body.link)
     .replace(`{{file}}`, req.body.file);
-  fs.writeFileSync(`./public/static/generated/index.html`, replacedIndex);
+  fs.writeFileSync(`./public/static/generated/index.html`, fixedIndex);
 
   const zip = new AdmZip();
   zip.addLocalFolder(`./public`);
   const date = new Date();
-  const hash =
-    date.getDay() +
-    "-" +
-    date.getMonth() +
-    "-" +
-    date.getFullYear() +
-    "-" +
-    date.getHours() +
-    "-" +
-    date.getMinutes() +
-    "-" +
-    date.getSeconds();
+  const hash = `${date.getFullYear()}-${date.getMonth()}-${date.getDay()}-${date.getHours()}-${date.getMinutes()}-${date.getSeconds()}`;
 
   zip.writeZip(`./public/static/extracted/preview-${hash}.zip`);
   const file = fs.readFileSync(`./public/static/generated/index.html`, "utf8");
